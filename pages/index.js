44ffa320e0a365d8779a7c6539a215e70437e9af
huna91 <gmyhy0510@naver.com>
2023-05-24 16:47:03 +0900

    dddd

--- pages/index.js
+++ pages/index.js
@@ -4,26 +4,32 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import Image from "next/image";
 
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>안녕하세요. 프론트엔드 개발자 윤하영 입니다.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
+      <div className={`${utilStyles.home_head_wrap}`}>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+        <section className={utilStyles.headingMd}>
+          <p>안녕하세요. 프론트엔드 개발자 윤하영 입니다.</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this in{" "}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+      </div>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
+            <li className={`${utilStyles.listItem}`} key={id}>
+              <Link href={`/posts/${id}`}>
+                <Image className={`${utilStyles.list_image}`} />
+                {title}
+              </Link>
               <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
@@ -32,7 +38,6 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
-      <button>글쓰기</button>
     </Layout>
   );
 }
