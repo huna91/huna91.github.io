eaf793c6f35d177cfc1bb5f5aeb0cf3994461dd3
huna91 <gmyhy0510@naver.com>
2023-05-29 16:14:40 +0900

    메일 정규식 폼 추가

--- pages/contacts/Email.js
+++ pages/contacts/Email.js
@@ -1,7 +1,7 @@
-import Link from "next/link";
-import { useRef, useState } from "react";
+import utilStyles from "../../styles/utils.module.css";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Mail_Ul, Mail_Li } from "../../styles/styledCom";
+import { Mail_input, Mail_Ul, Mail_Li, Mail_btn } from "../../styles/styledCom";
 
 const emails = [
   "@naver.com",
@@ -19,6 +19,7 @@ const Email = () => {
   const [emailList, setEmailList] = useState(emails);
   const [keyB, setKeyB] = useState(-1);
   const [drob, setDrop] = useState(false);
+  const [mailCheck, setMailCheck] = useState(false);
   const eRef = useRef();
 
   const control = (e) => {
@@ -29,15 +30,28 @@ const Email = () => {
       setEmailList(
         emails.filter((el) => el.includes(e.target.value.split("@")[1]))
       );
+      console.log(emailList);
     } else {
       setDrop(false);
       setKeyB(-1);
+    }
+    mail_check(e.target.value);
+  };
+  // 이메일 체크
+  const mail_check = (address) => {
+    const _checkData =
+      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (address.match(_checkData)) {
+      setMailCheck(true);
+    } else {
+      setMailCheck(false);
     }
   };
 
   // 드롭박스 데이터 클릭하면 인풋에 입력
   const handleDropDownClick = (first, second) => {
     _setAdd(`${first.split("@")[0]}${second}`);
+    mail_check(`${first.split("@")[0]}${second}`);
     setDrop(false);
     setKeyB(-1);
   };
@@ -47,10 +61,6 @@ const Email = () => {
       if (e.key === "ArrowDown" && emailList.length - 1 > keyB) {
         setKeyB(keyB + 1);
       }
-      //emailList.length에 -1을 해주는 이유는 selected의 최대값을 맞춰주기 위해서이다.
-      //예를들어 밑에 emailList 2개가 나왔다고 가정했을 때, selected값이 최대 1까지 변할 수 있게 해줘야한다.
-      //'ArrowDown'키를 누르면 selected는 0이 되고, 한번 더 누르면 1이 되고, 그 다음은 더이상 옵션이 없기 때문에 키가 안먹히게 해주는 것이다.
-
       if (e.key === "ArrowUp" && keyB >= 0) {
         setKeyB(keyB - 1);
       }
@@ -60,10 +70,20 @@ const Email = () => {
     }
   };
 
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (eRef.current && !eRef.current.contains(e.target)) {
+        setDrop(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+  }, [eRef]);
+
   return (
     <div>
-      <div ref={eRef}>
-        <input
+      <div className={utilStyles.email_input_container} ref={eRef}>
+        <Mail_input
           type="email"
           placeholder="이메일을 입력해주세요."
           value={_add}
@@ -71,51 +91,36 @@ const Email = () => {
             control(e);
           }}
           onKeyUp={handleKeyUp}
+          check={mailCheck}
         />
         {drob && (
           <Mail_Ul>
-            {emailList.map((item, idx) => {
+            {emailList.map((item, idx) => (
               <Mail_Li
                 key={idx}
                 onMouseOver={() => {
                   setKeyB(idx);
                 }}
                 onClick={() => handleDropDownClick(_add, item)}
-                onSelect={keyB === idx}
+                selected={keyB === idx}
               >
                 {_add.split("@")[0]}
                 {item}
-              </Mail_Li>;
-            })}
+              </Mail_Li>
+            ))}
           </Mail_Ul>
         )}
       </div>
-      {/* <input
-        onChange={(e) => {
-          console.log(e.currentTarget.value);
-          _setAdd(e.currentTarget.value);
-        }}
-        type="email"
-      />
-      <select
-        onChange={(e) => {
-          e.currentTarget.value === "직접입력"
-            ? setInputAct(true)
-            : setInputAct(false);
-          console.log(inputAct);
-        }}
-      >
-        <option>naver.com</option>
-        <option>gmail.com</option>
-        <option>직접입력</option>
-      </select>
-      <button
-        onClick={() => {
-          console.log(send_mail("kkkkkkkkkkkkkkkkkkkkkk"));
-        }}
-      >
-        이메일 보내기
-      </button> */}
+      <div>
+        <Mail_btn
+          able={mailCheck}
+          onClick={() => {
+            console.log("ddd");
+          }}
+        >
+          메일 전송
+        </Mail_btn>
+      </div>
     </div>
   );
 };
--- styles/styledCom.js
+++ styles/styledCom.js
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
-const Mail_Ul = styled.ul``;
+const Mail_input = styled.input`
+  box-shadow: 0 0 5px ${({ check }) => (check ? "green" : "pink")};
+  border: 1px solid ${({ check }) => (check ? "green" : "pink")};
+`;
+
+const Mail_Ul = styled.ul`
+  border: 1px solid red;
+`;
 
 const Mail_Li = styled.li`
   background-color: ${({ selected }) => (selected ? "#f5f5f5" : "")};
   color: ${({ selected }) => (selected ? "var(--zu--m4-color)" : "")};
 `;
 
-export { Mail_Ul, Mail_Li };
+const Mail_btn = styled.button`
+  opacity: ${({ able }) => (able ? 1 : 0.6)};
+  background-color: ${({ able }) => (able ? "blue" : "grey")};
+  cursor: pointer;
+`;
+/* cursor: ${({ able }) => (able ? pointer) : (not-allowed))}; */
+
+export { Mail_input, Mail_Ul, Mail_Li, Mail_btn };
