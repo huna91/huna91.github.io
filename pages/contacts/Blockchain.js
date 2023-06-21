12fe556f6625946d6f717bb7d5e1f72922bef5c1
huna91 <gmyhy0510@naver.com>
2023-06-21 16:37:14 +0900

    block ing

--- pages/contacts/Blockchain.js
+++ pages/contacts/Blockchain.js
@@ -1,11 +1,31 @@
-import React from "react";
-// import Send_btn from "../../components/button/send_btn";
+import React, { useState } from "react";
+import { encrypt, decrypt } from "../../lib/block";
 
 const Blockchain = () => {
+  const data = {
+    name: "윤하영",
+    password: "123",
+    contents: "hihi",
+  };
+
+  const [text, setText] = useState("d");
+  const [bb, setBB] = useState("bb");
+  const aaaa = () => {
+    const _text = encrypt(data, "secret");
+    setText(_text);
+  };
+  const bbbb = () => {
+    const _text = decrypt(text, "secret");
+    setBB(_text);
+  };
+
   return (
     <div style={{ position: "relative" }}>
-      Blockchain
-      {/* <Send_btn /> */}
+      원본:{JSON.stringify(data)}
+      <button onClick={aaaa}>암호화</button>
+      암호화:{text}
+      <button onClick={bbbb}>복호화</button>
+      복호화:{JSON.stringify(bb)}
     </div>
   );
 };
